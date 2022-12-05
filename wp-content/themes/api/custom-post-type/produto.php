004db8b9904681b9a132c40fc57f68ed578397e3
Geane Ferreira Pereira <geane3096@gmail.com>
2022-12-05 14:56:01 -0400

    subindo as alterações de hoje

--- wp-content/themes/api/custom-post-type/produto.php
+++ wp-content/themes/api/custom-post-type/produto.php
@@ -1,19 +1,17 @@
 <?php
-
-function registrar_cpt_produto(){
-    register_post_type('produto', array(
-        'label' => 'Produto',
-        'description' => 'Produto',
-        'public' => true,
-        'show_ui' => true,
-        'capability_type' => 'post',
-        'rewrite' => array('slug' => 'produto', 'width_front' => true),
-        'query_var' => true,
-        'supports' => array('custom-fields', 'author', 'title'),
-        'publicly_queryable' => true
-    ));
+function registrar_cpt_produto() {
+  register_post_type('produto', array(
+    'label' => 'Produto',
+    'description' => 'Produto',
+    'public' => true,
+    'show_ui' => true,
+    'capability_type' => 'post',
+    'rewrite' => array('slug' => 'produto', 'with_front' => true),
+    'query_var' => true,
+    'supports' => array('custom-fields', 'author', 'title'),
+    'publicly_queryable' => true
+  ));
 }
-
 add_action('init', 'registrar_cpt_produto');
 
 ?>
