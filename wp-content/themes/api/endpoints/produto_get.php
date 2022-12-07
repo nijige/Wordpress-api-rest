d1d5e054bf06837f1a82ae03c678070cc7e3c767
Geane Ferreira Pereira <geane3096@gmail.com>
2022-12-07 14:12:34 -0400

    refatorando(produto_get)

--- wp-content/themes/api/endpoints/produto_get.php
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -1,36 +1,121 @@
 <?php
 
-function api_usuario_get($request) {
-  $user = wp_get_current_user();
-  $user_id = $user->ID;
 
-  if($user_id > 0) {
-    $user_meta = get_user_meta($user_id);
 
-    $response = array(
-      "id" => $user->user_login,
-      "nome" => $user->display_name,
-      "email" => $user->user_email,
-      "cep" => $user_meta['cep'][0],
-      "numero" => $user_meta['numero'][0],
-      "rua" => $user_meta['rua'][0],
-      "bairro" => $user_meta['bairro'][0],
-      "cidade" => $user_meta['cidade'][0],
-      "estado" => $user_meta['estado'][0],
+function produto_scheme($slug)
+{
+    $post_id = get_produto_id_by_slug($slug);
+
+    if ($post_id) {
+        $post_meta = get_post_meta($post_id);
+
+        $images = get_attached_media('image', $post_id);
+
+        $images_array = null;
+
+        if ($images) {
+            $images_array = array();
+            foreach ($images as $key => $value) {
+                $images_array[] = array(
+                    'titulo' => $value->post_name,
+                    'src' => $value->guid,
+                );
+            }
+        }
+
+        $response = array(
+            "id" => $slug,
+            "fotos" => $images_array,
+            "nome" => $post_meta['nome'][0],
+            "preco" => $post_meta['preco'][0],
+            "descricao" => $post_meta['descricao'][0],
+            "vendido" => $post_meta['vendido'][0],
+            "usuario_id" => $post_meta['usuario_id'][0],
+
+        );
+    } else {
+        $response = new WP_Error('naoexiste', 'Produto não encontrado.',
+    }
+    return $response;
+}
+function api_produto_get($request)
+{
+    $response = produto_scheme($request["slug"]);
+    return rest_ensure_response($response);
+}
+function registrar_api_produto_get()
+{
+    register_rest_route('api', '/produto/(?P<slug>[-\w]+)', array(
+        array(
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => 'api_usuario_get',
+        ),
+    ));
+}
+
+add_action('rest_api_init', 'registrar_api_produto_get');
+
+
+// API PRODUTOS
+
+function api_produtos_($request){
+
+    $q = sanitize_text_field($request['q']) ?: '';
+    $_page = sanitize_text_field($request['_page']) ?: 0;
+    $_limit = sanitize_text_field($request['_limit']) ?: 9;
+    $usuario_id = sanitize_text_field($request['usuario_id']);
+
+    $usuario_id_query = null;
+    if ($usuario_id) {
+        $usuario_id_query = array(
+
+            'key' => 'usuario_id',
+            'value' => $usuario_id,
+            'compare' => '='
+
+        );
+    }
+
+    $vendido = array(
+        'key' => 'vendido',
+        'value' => 'false',
+        'compare' => '='
     );
-  } else {
-    $response = new WP_Error('permissao', 'Usuário não possui permissão', array('status' => 401));
-  }
-  return rest_ensure_response($response);
+
+    $query = array(
+        'post_type' => 'produto',
+        'post_per_page' => $_limit,
+        'paged' => $_page,
+        's' => $q,
+        'meta_query' => array(
+            $usuario_id_query,
+            $vendido,
+        )
+    );
+    
+    $loop = new WP_Query($query);
+    $posts = $loop->posts;
+    $total = $loop->found_posts;
+    
+    $produtos = array();
+    foreach ($posts as $key => $value) {
+        $produtos[] = produto_scheme($value->post_name);
+    }
+    $response = rest_ensure_response($produtos);
+    $response->header('X-Total-Count', $total);
+
+
+    return $response;
 }
 
-function registrar_api_usuario_get() {
-  register_rest_route('api', '/usuario', array(
-    array(
-      'methods' => WP_REST_Server::READABLE,
-      'callback' => 'api_usuario_get',
-    ),
-  ));
+function registrar_api_produtos_get()
+{
+    register_rest_route('api', '/produto', array(
+        array(
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => 'api_usuario_get',
+        ),
+    ));
 }
 
-add_action('rest_api_init', 'registrar_api_usuario_get');
+add_action('rest_api_init', 'registrar_api_produto_get');
