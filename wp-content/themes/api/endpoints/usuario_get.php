38c6a03a1c7d55b64294891d615fb40d7b78c4b2
Geane Ferreira Pereira <geane3096@gmail.com>
2022-12-07 11:56:04 -0400

    refatorando usuario_get

--- wp-content/themes/api/endpoints/usuario_get.php
+++ wp-content/themes/api/endpoints/usuario_get.php
@@ -1,59 +1,38 @@
 <?php
-// API PRODUTOS
-function api_transacao_get($request) {
-  $tipo = sanitize_text_field($request['tipo']) ?: 'comprador_id';
+
+function api_usuario_get($request)
+{
   $user = wp_get_current_user();
   $user_id = $user->ID;
 
-  if($user_id) {
-    $login = get_userdata($user_id)->user_login;
+  if ($user_id > 0) {
+    $user_meta = get_user_meta($user_id);
 
-    $meta_query = null;
-    if($tipo) {
-      $meta_query = array(
-        'key' => $tipo,
-        'value' => $login,
-        'compare' => '='
-      );
-    }
-
-    $query = array(
-      'post_type' => 'transacao',
-      'orderby' => 'date',
-      'posts_per_page' => -1,
-      'meta_query' => array(
-        $meta_query
-      )
+    $response = array(
+      "id" => $user->user_login,
+      "nome" => $user->display_name,
+      "email" => $user->user_email,
+      "cep" => $user_meta['cep'][0],
+      "numero" => $user_meta['numero'][0],
+      "rua" => $user_meta['rua'][0],
+      "bairro" => $user_meta['bairro'][0],
+      "cidade" => $user_meta['cidade'][0],
+      "estado" => $user_meta['estado'][0],
     );
-
-    $loop = new WP_Query($query);
-    $posts = $loop->posts;
-
-    $response = array();
-    foreach ($posts as $key => $value) {
-      $post_id = $value->ID;
-      $post_meta = get_post_meta($post_id);
-
-      $response[] = array(
-        'comprador_id' => $post_meta['comprador_id'][0],
-        'vendedor_id' => $post_meta['vendedor_id'][0],
-        'endereco' => json_decode($post_meta['endereco'][0]),
-        'produto' => json_decode($post_meta['produto'][0]),
-        'data' => $value->post_date,
-      );
-    }
   } else {
-    $response = new WP_error('permissao', 'Usuário não possui permissão.', array('status' => 401));
+    $response = new WP_Error('permissao', 'Usuário não possui permissão', array('status' => 401));
   }
   return rest_ensure_response($response);
 }
 
-function registrar_api_transacao_get() {
-  register_rest_route('api', '/transacao', array(
+function registrar_api_usuario_get()
+{
+  register_rest_route('api', '/usuario', array(
     array(
       'methods' => WP_REST_Server::READABLE,
-      'callback' => 'api_transacao_get',
+      'callback' => 'api_usuario_get',
     ),
   ));
 }
-add_action('rest_api_init', 'registrar_api_transacao_get');
+
+add_action('rest_api_init', 'registrar_api_usuario_get');
